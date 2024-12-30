65d28fc1ac2fc6ad1467b358727fa3794d18560b
dany <holodnayazvezda@yandex.ru>
2024-12-30 03:16:52 +0300

    Update ua config

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
-from fake_useragent import UserAgent
+import random
 
 MAIN_URL = 'https://reshak.ru'
 
-ua = UserAgent(browsers=["chrome", "firefox", "safari"])
+ua = [
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+    'Opera/9.80 (Macintosh; Intel Mac OS X; U; en) Presto/2.2.15 Version/10.00'
+]
 
 
 async def get_headers():
     return {
-        'User-Agent': ua.random,
+        'User-Agent': random.choice(ua),
         'Host': 'reshak.ru',
         'Accept': '*/*',
     }
